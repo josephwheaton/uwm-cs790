c56bd816286b7f84da7440e77834900ea843a73a
Joseph Wheaton <joe.wheaton@advicent.com>
2020-03-01 00:10:44 -0600

    way too much effort for very early part of hw

--- hwk5/hwk5.hs
+++ hwk5/hwk5.hs
@@ -1,59 +1,66 @@
 import Data.Complex
 import Data.Time
 import GHC.Base (liftA2)
+
+-- todo: Reuse type-class instances of Vec, defining Vec as below:
 newtype Vec a = Vec {runVec :: [a]}
 
--- todo: 1. We will use the tyupe alias Signal
+-- todo: Reader monad
+-- ! todo figure out how this works
+newtype Reader r a = Reader {runReader :: r -> a}
+
+-- todo: 1. We will use the type alias Signal
 type Signal = Vec (Complex Double)
 
 -- todo: 2. Provided type class instances
--- ! replaced provided show with my own implementation
 instance Show a => Show (Vec a) where
-  show (Vec a) = "[" ++ (unwords $ show <$> a) ++ "]"
+  show (Vec lst) = "[" ++ drop 1 lst' ++ "]"
+    where lst' = mconcat $ map (\x -> " " ++ show x) lst
+
 
 instance Functor Vec where
   fmap f (Vec a) = Vec $ map f a
 
 instance Applicative Vec where 
-  pure = Vec . repeat 
-  (Vec f) <*> (Vec x) = Vec $ map (uncurry ($)) $ zip f x 
+  pure = Vec . repeat
+  (Vec f) <*> (Vec x) = Vec $ map (uncurry ($)) $ zip f x
   liftA2 f (Vec x) (Vec y) = Vec $ map (uncurry f) $ zip x y
 
-instance Num a => Num (Vec a) where 
-  (+) = liftA2 (+) 
-  (-) = liftA2 (-) 
-  (*) = liftA2 (*) 
-  negate = fmap negate 
-  abs = fmap abs 
-  signum = fmap signum 
+instance Num a => Num (Vec a) where
+  (+) = liftA2 (+)
+  (-) = liftA2 (-)
+  (*) = liftA2 (*)
+  negate = fmap negate
+  abs = fmap abs
+  signum = fmap signum
   fromInteger x = pure $ fromInteger x
 
-instance (Floating a) => Fractional (Vec a) where 
-  (/) = liftA2 (/) 
-  fromRational x = pure $ fromRational
+instance (Floating a) => Fractional (Vec a) where
+  (/) = liftA2 (/)
+  fromRational x = pure $ fromRational x
 
-instance (Floating a) => Floating (Vec a) where 
-  pi = pure pi 
-  exp = fmap exp 
-  log = fmap log 
-  sin = fmap sin 
-  cos = fmap cos 
-  asin = fmap asin 
-  acos = fmap acos 
-  atan = fmap atan 
-  sinh = fmap sinh 
-  cosh = fmap cosh 
-  asinh = fmap asinh 
-  acosh = fmap acosh 
+instance (Floating a) => Floating (Vec a) where
+  pi = pure pi
+  exp = fmap exp
+  log = fmap log
+  sin = fmap sin
+  cos = fmap cos
+  asin = fmap asin
+  acos = fmap acos
+  atan = fmap atan
+  sinh = fmap sinh
+  cosh = fmap cosh
+  asinh = fmap asinh
+  acosh = fmap acosh
   atanh = fmap atan
 
-instance Foldable Vec where 
+instance Foldable Vec where
   foldr f c (Vec a) = foldr f c a
 
-imagV :: Num a => Vec a -> Vec (Complex a) 
+imagV :: Num a => Vec a -> Vec (Complex a)
 imagV (Vec a) = Vec $ map (0:+) a
 
-realV :: Num a => Vec a -> Vec (Complex a) 
+realV :: Num a => Vec a -> Vec (Complex a)
 realV (Vec a) = Vec $ map (:+0) a
 
 instance Semigroup (Vec a) where Vec a <> Vec b = Vec $ a++b
@@ -61,58 +68,89 @@ instance Semigroup (Vec a) where Vec a <> Vec b = Vec $ a++b
 instance Monoid (Vec a) where mempty = Vec []
 
 -- todo: 3. Provided auxiliary functions
-range :: Double -> Double -> Double -> [Double] 
-range from to count = map (\x -> from + x * step) [0..count-1] 
-  where step = (to - from)/count
+range :: Double -> Double -> Double -> [Double]
+range from to count = map (\x -> from + x * step) [0..count-1]
+  where step = (to - from) / count
 
-absolute :: Vec (Complex Double) -> Vec Double 
+absolute :: Vec (Complex Double) -> Vec Double
 absolute = fmap (\(r:+i) -> sqrt(r*r + i*i))
 
-rd :: Int -> Vec Double -> Vec Double 
-rd n = fmap (\x -> fromIntegral (round $ c * x) / c) 
+rd :: Int -> Vec Double -> Vec Double
+rd n = fmap (\x -> fromIntegral (round $ c * x) / c)
   where c = 10^n
 
-length’ (Vec x) = length x
+length' :: Vec a -> Int
+length' (Vec x) = length x
 
 -- todo: 4. Implement a function to calculate the twiddle factor for DFT
 -- todo: 4. a) implement twiddle
 twiddle :: Double -> Double -> Signal
-
+twiddle n k = 
+  let 
+    ns = range 0 n n
+    y = -2.0 * pi * k / n
+  in
+    Vec $ (\ni -> exp(0.0 :+ (ni * y))) <$> ns
 
 -- todo: 4. b) use twiddle to implement DFT
 dft :: Signal -> Signal
-
+dft v = ((\tf -> sum (v * tf)) <$> tfs)
+  where 
+    n :: Double
+    n = fromIntegral $ length' v
+    ns :: Vec Double
+    ns = Vec $ range 0 n n
+    tfs :: Vec (Signal)
+    tfs = (twiddle n) <$> ns -- ? this will generate Vector of Vectors
 
 -- todo: 4. c) implement inverse DFT
 idft :: Signal -> Signal
+idft v = (/(n :+ 0.0)) <$> conjugate <$> dft (conjugate <$> v)
+  where
+    n :: Double
+    n = fromIntegral $ length' v
 
 
 -- todo: 5. Implement a low-pass filter using DFT and inverse DFT
 -- todo: 5. a) implement mask
-mask :: Int -> Int -> Signa
-
+mask :: Int -> Int -> Signal
+mask freq n = 
+  realV $ Vec $ one ++ zero ++ one
+    where one = (take freq $ repeat 1)
+          zero = (take (n - freq * 2) $ repeat 0)
 
 -- todo: 5. b) implement low_pass'
-low_pass’ :: Int -> Signal -> Signal
-
+low_pass' :: Int -> Signal -> Signal
+low_pass' freq v = 
+  let n = length' v 
+  in idft (dft v * mask freq n)
 
 -- todo: 6. Implement FFT and IFFT, reusing the twiddle factor(s)
 -- todo: 6. a) fft
 fft :: Signal -> Reader Signal Signal
+fft v = mempty
 
 
 -- todo: 6. b) ifft
 ifft :: Signal -> Reader Signal Signal
-
+ifft v = (/(n :+ 0.0)) <$> conjugate <$> fft (conjugate <$> v)
+  where
+    n :: Double
+    n = fromIntegral $ length' v
 
 -- todo: 6. c) Implement low_pass using the new fft and ifft
 low_pass :: Int -> Signal -> Signal
-
+low_pass freq v =
+  let n = length' v
+  in ifft (fft v * mask freq n)
 
 -- todo: Testing
-main = do 
-  let n = fromIntegral 2^8 
-  let s1 = fmap (\x -> sin(20*pi*x) + sin(40*pi*x)/2) $ Vec $ range 0 1 n 
+main = do
+  let n = fromIntegral 2^8
+  let s1 = fmap (\x -> sin(20*pi*x) + sin(40*pi*x)/2) $ Vec $ range 0 1 n
   print(rd 3 s1)
-  print(rd 3 $ fmap (\(r:+_) -> r) $ low_pass’ 15 $ realV s1)
-  print(rd 3 $ fmap (\(r:+_) -> r) $ low_pass 15 $ realV s1)
+
+  print(dft $ realV s1)
+  print(mask 14 (length' s1))
+  print(dft $ realV s1 * mask 15 (length' s1))
+  print(rd 3 $ fmap (\(r:+_) -> r) $ low_pass' 15 $ realV s1)

f28cd8d61d61d25392f4b5b0484bec200728d291
Joseph Wheaton <joe.wheaton@advicent.com>
2020-04-26 19:33:42 -0500

    hwk9

--- hwk9/hwk9.hs
+++ hwk9/hwk9.hs
@@ -1,42 +1,54 @@
-import Control.Concurrent
-import Control.Concurrent.Async
+{-# LANGUAGE PackageImports #-}
+
+import Control.Concurrent (newEmptyMVar, putMVar, takeMVar)
+import "async" Control.Concurrent.Async (async, cancel, wait)
 import Control.Exception (catch, displayException, SomeException(..))
 import qualified Data.ByteString.Lazy as L
-import Network.HTTP.Conduit
+import "http-conduit" Network.HTTP.Conduit
+import Control.Monad (unless)
 
 finish :: [IO () -> IO b] -- a list of actions, each takes a canceller IO monad
        -> Int             -- the number of actions that are allowed to complete
-       -> IO [b]          -- output messages of all actionsx
-finish actions n =
-  do  recv <- newEmptyMVar
-      send <- newEmptyMVar
-      let runAction (f, id) = async $ f $ putMVar recv id >> takeMVar send
-      threads <- mapM runAction $ zip actions [1..]
+       -> IO [b]          -- output messages of all actions
+finish actions count = do
+  -- ? Initialize `MVar`s
+  currentAction <- newEmptyMVar
+  counter <- newEmptyMVar
+  
+  -- runAction :: (IO () -> IO a, Integer) -> IO (Async a)
+  let runAction (action, actionId) = async $ action $ putMVar currentAction actionId >> takeMVar counter
 
-      let g ids = if length ids < n
-        then do id <- takeMVar recv
-          putMVar send ()
-          g (id:ids)
-        else mapM_ f $ zip threads [1..]
-          where f (t, id) = unless (id `elem` lst) $ cancel t
-        g []
-        mapM wait threads
+  threads <- mapM runAction $ zip actions [1..]
 
+  -- raceThreads :: [Integer] -> IO ()
+  let raceThreads actionIds = 
+        if length actionIds < count
+        then do 
+          actionId <- takeMVar currentAction
+          putMVar counter ()
+          raceThreads (actionId:actionIds)
+        else mapM_ cancelThread $ zip threads [1..]
+          -- cancelThread :: (Async a, Integer) -> IO ()
+          where cancelThread (action, actionId) = unless (actionId `elem` actionIds) (cancel action)
+  raceThreads []
+  mapM wait threads
+
+main :: IO ()
 main = do
   let urls = [("http://www.yahoo.com/", "test1.txt"),
               ("http://www.google.com/", "test2.txt"),
               ("http://www.msn.com/", "test3.txt"),
-              ("http://www.bing.com/", "test4.txt")]]
+              ("http://www.bing.com/", "test4.txt")]
 
   let actions = map request urls
   ms <- finish actions 2
   mapM_ putStrLn ms
-where
-  request :: (String, String) -> IO () -> IO String
-  request (url, f) canceller = do
-    d <- simpleHttp url
-    canceller
-    L.writeFile f d
-    return $ url ++ " done"
-    'catch' handler
-    where handle = \(SomeException e) -> return (url ++ ": " ++ displayException e)
+  where
+    request :: (String, String) -> IO () -> IO String
+    request (url, f) canceller = do
+      d <- simpleHttp url
+      canceller
+      L.writeFile f d
+      return $ url ++ " done"
+      `catch` handler
+      where handler = \(SomeException e) -> return (url ++ ": " ++ displayException e)
